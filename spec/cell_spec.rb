970322bad4ce3c996236c1295d40a63549cf077b
Amanda Ross <amikaross@gmail.com>
2022-08-30 08:47:23 -0600

    Fix before:each to accomodate #render tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,59 +3,68 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe Cell do 
-# Setup of all tests (create a ship and cell)
+# Setup for all tests (create a ship and cell)
   before(:each) do 
     @cruiser = Ship.new("Cruiser", 3)
-    @cell = Cell.new("B4")
+    @cell_1 = Cell.new("B4")
   end
 
   describe "#initialize" do 
     it "is an instance of Cell" do 
-      expect(@cell).to be_an_instance_of(Cell)
+      expect(@cell_1).to be_an_instance_of(Cell)
     end
 
     it "has readable coordinate and ship attributes" do 
-      expect(@cell.coordinate).to eq "B4"
-      expect(@cell.ship).to eq nil 
+      expect(@cell_1.coordinate).to eq "B4"
+      expect(@cell_1.ship).to eq nil 
     end
   end
 
   describe "#empty?" do 
     it "starts out empty by default" do 
-      expect(@cell.ship).to eq nil 
-      expect(@cell.empty?).to eq true 
+      expect(@cell_1.ship).to eq nil 
+      expect(@cell_1.empty?).to eq true 
     end
 
     it "returns false when a ship is on cell" do 
-      @cell.place_ship(@cruiser)
-      expect(@cell.empty?).to eq false 
+      @cell_1.place_ship(@cruiser)
+      expect(@cell_1.empty?).to eq false 
     end
   end
 
   describe "#place_ship" do 
     it "updates the ship attribute correctly when called" do 
-      expect(@cell.ship).to eq nil 
-      @cell.place_ship(@cruiser)
-      expect(@cell.ship).to eq @cruiser 
+      expect(@cell_1.ship).to eq nil 
+      @cell_1.place_ship(@cruiser)
+      expect(@cell_1.ship).to eq @cruiser 
     end
   end 
 
   describe "#fire_upon" do 
     it "reduces ship health by 1" do 
-      @cell.place_ship(@cruiser)
-      expect(@cell.ship.health).to eq 3 
-      @cell.fired_upon
-      expect(@cell.ship.health).to eq 2 
+      @cell_1.place_ship(@cruiser)
+      expect(@cell_1.ship.health).to eq 3 
+      @cell_1.fire_upon
+      expect(@cell_1.ship.health).to eq 2 
     end
   end
 
   describe "#fired_upon?" do 
-    it "knows when its been fired upon" do 
-      @cell.place_ship(@cruiser)
+    it "knows when its been fired upon when a ship is present" do 
+      @cell_1.place_ship(@cruiser)
       expect(@cell.fired_upon?).to eq false
-      @cell.fire_upon 
-      expect(@cell.fired_upon?).to eq true 
+      @cell_1.fire_upon 
+      expect(@cell_1.fired_upon?).to eq true 
     end
+
+    it "knows when a ship is not present"
+    expect(@cell_1.fired_upon?).to eq false
+    @cell_1.fire_upon 
+    expect(@cell_1.fired_upon?).to eq true 
+  end
+
+  describe "#render" do 
+
   end
 
 end
