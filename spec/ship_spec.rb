d30ac9a60e9d96681a2ba6a3c009039e6cb49b16
Annie Pulzone <ajpulzone@gmail.com>
2022-08-30 08:01:57 -0700

    Initialize ship, add hit and sunk? methods

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,39 +3,39 @@ require './lib/ship'
 
 RSpec.describe Ship do
 # Setup for all tests (create a Ship)
-  before(:each) do 
+  before(:each) do
     @cruiser = Ship.new("Cruiser", 3)
   end
-  describe "#initialize" do 
+  describe "#initialize" do
     it "is an instance of Ship" do
       expect(@cruiser).to be_an_instance_of(Ship)
     end
 
-    it "has a readable name, length, and health" do 
+    it "has a readable name, length, and health" do
       expect(@cruiser.name).to eq "Cruiser"
       expect(@cruiser.length).to eq 3
       expect(@cruiser.health).to eq 3
     end
   end
 
-  describe "#hit" do 
+  describe "#hit" do
     it "reduces the health of the ship by 1" do
       expect(@cruiser.health).to eq 3
-      @cruiser.hit 
-      expect(@cruiser.health).to eq 2 
+      @cruiser.hit
+      expect(@cruiser.health).to eq 2
       @cruiser.hit
       expect(@cruiser.health).to eq 1
     end
   end
 
-  describe "#sunk?" do 
-    it "returns true only if ship health is 0" do 
+  describe "#sunk?" do
+    it "returns true only if ship health is 0" do
       expect(@cruiser.sunk?).to eq false
       2.times { @cruiser.hit }
-      expect(@cruiser.sunk?).to eq false 
+      expect(@cruiser.sunk?).to eq false
       @cruiser.hit
       expect(@cruiser.health).to eq 0
-      expect(@cruiser.sunk?).to eq true 
+      expect(@cruiser.sunk?).to eq true
     end
   end
 end
