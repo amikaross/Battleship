31852c4ad64916638aafba509c3355ee219d132b
Annie Pulzone <ajpulzone@gmail.com>
2022-08-30 10:48:39 -0700

    Instance of cell test passed, code written

---
+++ lib/cell.rb
@@ -0,0 +1,21 @@
+class Cell
+  attr_reader :cell, :coordinate, :ship
+
+  def initialize(cell)
+    @cell = cell
+    @ship = ship
+    @coordinate = coordinate
+
+  end
+  #
+  # def empty?
+  #
+  # end
+  #
+  # def place_ship(cruiser)
+  #
+  # end
+
+
+
+end
