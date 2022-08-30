37a9b3fa754d2822e410ea02d97d98164aada7a4
Amanda Ross <amikaross@gmail.com>
2022-08-30 08:07:05 -0600

    Update ship test parameters

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require './lib/ship'
 
-RSpec.describe Card do
+RSpec.describe Ship do
 # Setup for all tests (create a Ship)
   before(:each) do 
     @cruiser = Ship.new("Cruiser", 3)
@@ -13,13 +13,14 @@ RSpec.describe Card do
 
     it "has a readable name, length, and health" do 
       expect(@cruiser.name).to eq "Cruiser"
-      expect(@cruiser.legnth).to eq 3
+      expect(@cruiser.length).to eq 3
       expect(@cruiser.health).to eq 3
     end
   end
 
   describe "#hit" do 
     it "reduces the health of the ship by 1" do
+      expect(@cruiser.health).to eq 3
       @cruiser.hit 
       expect(@cruiser.health).to eq 2 
       @cruiser.hit
@@ -33,6 +34,7 @@ RSpec.describe Card do
       2.times { @cruiser.hit }
       expect(@cruiser.sunk?).to eq false 
       @cruiser.hit
+      expect(@cruiser.health).to eq 0
       expect(@cruiser.sunk?).to eq true 
     end
   end
