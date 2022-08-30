e9a3cfdf4f267ed59d5ae6ab682df5eeed75b679
Amanda Ross <amikaross@gmail.com>
2022-08-30 08:35:38 -0600

    Add #place_ship tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -10,26 +10,33 @@ RSpec.describe Cell do
   end
 
   describe "#initialize" do 
-    it "is an instance of Cell" do 
+    xit "is an instance of Cell" do 
       expect(@cell).to be_an_instance_of(Cell)
     end
 
-    it "has readable coordinate and ship attributes" do 
+    xit "has readable coordinate and ship attributes" do 
       expect(@cell.coordinate).to eq "B4"
       expect(@cell.ship).to eq nil 
     end
   end
-end
 
   describe "#empty?" do 
-    it "starts out empty by default" do 
+    xit "starts out empty by default" do 
       expect(@cell.ship).to eq nil 
       expect(@cell.empty?).to eq true 
     end
 
-    it "returns false when ship is on cell" do 
+    xit "returns false when ship is on cell" do 
       @cell.place_ship(@cruiser)
       expect(@cell.empty?).to eq false 
     end
   end
+
+  describe "#place_ship" do 
+    xit "updates the ship attribute correctly when called" do 
+      expect(@cell.ship).to eq nil 
+      @cell.place_ship(@cruiser)
+      expect(@cell.ship).to eq @cruiser 
+    end
+  end 
 end
