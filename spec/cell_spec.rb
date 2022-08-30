4a445894684cd36be9193eb6850a0685b0b13409
Amanda Ross <amikaross@gmail.com>
2022-08-30 08:32:22 -0600

    Add cell #initialize tests

---
+++ spec/cell_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do 
+# Setup of all tests (create a ship and cell)
+  before(:each) do 
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell = Cell.new("B4")
+  end
+
+  describe "#initialize" do 
+    it "is an instance of Cell" do 
+      expect(@cell).to be_an_instance_of(Cell)
+    end
+
+    it "has readable coordinate and ship attributes" do 
+      expect(@cell.coordinate).to eq "B4"
+      expect(@cell.ship).to eq nil 
+    end
+  end
+end
