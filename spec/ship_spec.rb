d6cd44d6fb3e0e1345c6b6097d91a28f2d5171ec
Amanda Ross <amikaross@gmail.com>
2022-08-30 05:49:56 -0600

    Add tests for ship class

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,39 @@
-#Created ship_spec.rb file
+require 'rspec'
+require './lib/ship'
+
+RSpec.describe Card do
+# Setup for all tests (create a Ship)
+  before(:each) do 
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+  describe "#initialize" do 
+    it "is an instance of Ship" do
+      expect(@cruiser).to be_an_instance_of(Ship)
+    end
+
+    it "has a readable name, length, and health" do 
+      expect(@cruiser.name).to eq "Cruiser"
+      expect(@cruiser.legnth).to eq 3
+      expect(@cruiser.health).to eq 3
+    end
+  end
+
+  describe "#hit" do 
+    it "reduces the health of the ship by 1" do
+      @cruiser.hit 
+      expect(@cruiser.health).to eq 2 
+      @cruiser.hit
+      expect(@cruiser.health).to eq 1
+    end
+  end
+
+  describe "#sunk?" do 
+    it "returns true only if ship health is 0" do 
+      expect(@cruiser.sunk?).to eq false
+      2.times { @cruiser.hit }
+      expect(@cruiser.sunk?).to eq false 
+      @cruiser.hit
+      expect(@cruiser.sunk?).to eq true 
+    end
+  end
+end
