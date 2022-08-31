fc0a080a73203ab7b4b38193c78bdaef0f408464
Annie Pulzone <ajpulzone@gmail.com>
2022-08-31 10:55:38 -0700

    Feature: add RSpec describe and required files

---
+++ spec/board_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/cell' #may not need this, may be accessible through the ship file
+require './lib/ship'
+
+RSpec.describe Board do
+
+  before(:each) do
+    
+  end
+
+  describe "" do
+    it "" do
+
+    end
+  end
+
+end
