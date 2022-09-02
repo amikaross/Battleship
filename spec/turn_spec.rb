ab24adbd668fc55ca79bd20515b7a3594fefd04e
Annie Pulzone <ajpulzone@gmail.com>
2022-09-02 16:35:08 -0700

    Feature: comment out describe results test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,34 +1,34 @@
 require 'rspec'
 require './lib/turn'
 
-RSpec.describe Turn do 
-  before(:each) do 
-    @player_board = Board.new 
+RSpec.describe Turn do
+  before(:each) do
+    @player_board = Board.new
     @computer_board = Board.new
     @turn = Turn.new(@player_board, @computer_board)
   end
 
-  describe "#initialize" do 
-    it "exists" do 
+  describe "#initialize" do
+    it "exists" do
       expect(@turn).to be_an_instance_of(Turn)
     end
   end
 
-  describe "#player_shot" do 
-    it "" do 
+  describe "#player_shot" do
+    it "Should update the cell object with correct fired upon status" do
 
     end
   end
 
-  describe "#computer_shot" do 
-    it "" do 
+  describe "#computer_shot" do
+    it "Should update the cell object with correct fired upon status" do
 
     end
   end
 
-  describe "#results" do 
-    it "" do 
-
-    end
-  end
+  # describe "#results" do
+  #   it "" do
+  #     #terminal output, may not be able to test
+  #   end
+  # end
 end
