49653cf4f34e8c8fa17ad68ff68bcd606c8d0e36
Amanda Ross <amikaross@gmail.com>
2022-09-02 15:07:14 -0600

    Create turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+require './lib/board'
+
+class Turn 
+  attr_reader :player_board, 
+              :computer_board
+
+  def initialize(player_board, computer_board)
+    @player_board = player_board
+    @computer_board = computer_board 
+  end
+end
