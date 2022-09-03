79518417370ab6474072c69f626fd9415af4c0cc
Amanda Ross <amikaross@gmail.com>
2022-09-03 16:46:46 -0600

    Add additional tests for player_fires and computer_fires

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -27,23 +27,67 @@ RSpec.describe Turn do
     end
   end
 
-  describe "#player_shot" do
+  describe "#player_fires" do
     it "Should update fired_upon status of a cell object on the computer_board" do
-      expect(@turn.computer_board.cells.values.all? { |cell| cell.fired_upon? == false}).to be(true)
+      expect(@turn.computer_board.cells.values.all? { |cell| cell.fired_upon? == false }).to eq(true)
       @turn.player_shot("A1")
       expect(@turn.computer_board.cells["A1"].fired_upon?).to eq(true)
+      @turn.player_shot("B4")
+      expect(@turn.computer_board.cells["B4"].fired_upon?).to eq(true)
+    end
+
+    it "should return the updated computer_board object" do 
+      expect(@turn.player_fires).to eq(@turn.computer_board)
     end
   end
 
-  describe "#computer_shot" do
+  describe "#computer_fires" do
     it "Should update the cell object on the player's board with correct fired upon status" do
+      expect(@turn.player_board.cells.valuess.all? { |cell| cell.fired_upon == false }).to eq(true)
+      @turn.computer_shot("A1")
+      expect(@turn.player_board.cells["A1"].fired_upon?).to eq(true)
+      @turn.computer_shot("B3")
+      expect(@turn.player_board.cells["A1"].fired_upon?).to eq(true)
+    end
 
+    it "should return the updated player_board object" do
+      expect(@turn.computer_fires).to eq (@turn.player_board)
     end
   end
 
-  # describe "#results" do
-  #   it "" do
-  #     #terminal output, may not be able to test
-  #   end
+  # describe "#get_player_shot" do 
+  #   may not be able to test - user input
   # end
+
+  describe "#random_shot" do 
+    it "returns a single valid coordinate" do 
+      shot = @turn.random_shot
+      expect(@turn.player_board.valid_coordinate?(shot)).to eq(true)
+    end
+
+    it "only returns a coordinate that hasn't already been fired upon" do 
+      @turn.@player_board.cells["A1"].fire_upon
+      @turn.@player_board.cells["A2"].fire_upon
+      @turn.@player_board.cells["A3"].fire_upon
+      @turn.@player_board.cells["A4"].fire_upon
+      @turn.@player_board.cells["B1"].fire_upon
+      @turn.@player_board.cells["B2"].fire_upon
+      @turn.@player_board.cells["B3"].fire_upon
+      @turn.@player_board.cells["B4"].fire_upon
+      @turn.@player_board.cells["C1"].fire_upon
+      @turn.@player_board.cells["C2"].fire_upon
+      @turn.@player_board.cells["C3"].fire_upon
+      @turn.@player_board.cells["C4"].fire_upon
+      @turn.@player_board.cells["D1"].fire_upon
+      @turn.@player_board.cells["D2"].fire_upon
+      @turn.@player_board.cells["D3"].fire_upon
+      expect(@turn.random_shot).to eq("D4")
+    end
+
+    it "returns a different random coordinate every time called" do
+      shot_1 = @turn.random_shot
+      shot_2 = @turn.random_shot 
+      expect(shot_1 != shot_2)
+    end
+  end
 end
